cd5ddbfcec07ae24b1ba62f52c11a23bad74c968
Katya_Horton <you@example.com>
2019-11-19 13:33:55 +0800

    chore, add dialog

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PropertyContainer from "./containers/PropertyContainer.js";
 import { PropertiesContextProvider } from "./context/propertiesContext.js";
+import FavPropertyContainer from "./containers/FavPropertiesContainer.js";
 
 function App() {
   return (
     <PropertiesContextProvider>
+      <FavPropertyContainer />
       <PropertyContainer />
     </PropertiesContextProvider>
   );
