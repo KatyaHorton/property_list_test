2da48f4a151fd912f80879310379bbb608bcc180
Katya_Horton <you@example.com>
2019-11-19 08:25:24 +0800

    chore: test1

--- src/containers/PropertyContainer.js
+++ src/containers/PropertyContainer.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fecthPropertyList } from "../services/propertySerivices";
 
 const PropertyContainer = () => {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    async function getList(fn) {
+      const result = await fecthPropertyList();
+      const newProperties = await result;
+      fn(newProperties);
+    }
+    getList(setProperties);
+  }, []);
+
   return (
     <article>
       <ol>
-        <li>1. test</li>
-        <li>2. test</li>
-        <li>3. test</li>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+        {/* {properties &&
+          properties.map(peoperty => <li>{peoperty.buildingName}</li>)} */}
       </ol>
     </article>
   );
