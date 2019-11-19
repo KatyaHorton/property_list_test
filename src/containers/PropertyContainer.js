7a1557ae0442c062646df3c21242204e576da460
Katya_Horton <you@example.com>
2019-11-19 08:18:44 +0800

    test: circleci

---
+++ src/containers/PropertyContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PropertyContainer = () => {
+  return (
+    <article>
+      <ol>
+        <li>test</li>
+        <li>test</li>
+        <li>test</li>
+      </ol>
+    </article>
+  );
+};
+
+export default PropertyContainer;
